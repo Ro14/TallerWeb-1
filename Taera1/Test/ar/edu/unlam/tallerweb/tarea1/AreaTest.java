d1ae2b1757fd0cedc89c2e6732d167143b4aae12
Ro14 <specialneeds_Ro@hotmail.com>
2016-04-13 23:57:23 -0300

    Update AreaTest.java
    Test probado y funcionando

--- Taera1/Test/ar/edu/unlam/tallerweb/tarea1/AreaTest.java
+++ Taera1/Test/ar/edu/unlam/tallerweb/tarea1/AreaTest.java
@@ -1,23 +1,38 @@
 package ar.edu.unlam.tallerweb.tarea1;
 
 import org.junit.Test;
+import org.junit.Assert;
 
 public class AreaTest {
 
+	@SuppressWarnings("deprecation")
 	@org.junit.Test
 	public void testAreaCuadrado() {
 		Area miArea= new Area();
 		Integer lado=7;
-		miArea.AreaCuadrado(lado);
+		float areaEsperada=49;
+		
+		float areaObtenida=miArea.AreaCuadrado(lado);
+		
+		Assert.assertEquals(areaEsperada,areaObtenida,0.1);
+		
+		miArea=null;
 		
 	}
 	
 	@Test
+	
 	public void testAreaCirculo()
 	{
 		Area miArea=new Area();
-		Integer radio=15;
-		miArea.AreaCirculo(radio);
+		Integer radio=3;
+		float areaEsperada=28.26F;
+		
+		float areaObtenida=miArea.AreaCirculo(radio);
+		
+		Assert.assertEquals(areaEsperada,areaObtenida,0.1);
+		
+		miArea=null;
 		
 	}
 	
